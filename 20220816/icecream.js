4cc6993b07fee14b6e0050b86b5c7027e8749ac2
MAGEUNWON <109570530+MAGEUNWON@users.noreply.github.com>
2022-08-16 18:06:44 +0900

    Add files via upload

---
+++ 20220816/icecream.js
@@ -0,0 +1,25 @@
+let members = 29;
+let price = 1000;
+let add = 2000;
+let result = (members * price) + add;
+
+function icecreamPrice(members, price, add){
+  return (members * price)+add;
+}
+
+console.log(icecreamPrice(29,1000,2000));
+
+// let month = 1670000;
+// let year = 12;
+// let tax = 3;
+// let result = (month*year)*tax;
+
+// function i(month, year, tax){
+//   return (month*year)*tax;
+// }
+
+// console.log(i(1670000, 12, 3));
+
+
+
+
